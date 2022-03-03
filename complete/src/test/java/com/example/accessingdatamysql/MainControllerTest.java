799a1bf4dc754bf0b2c17a2e85955573aa0658f5
Jonny Benj <80647447+linuxfjb@users.noreply.github.com>
2022-03-03 00:21:06 -0500

    deployment fail test

--- complete/src/test/java/com/example/accessingdatamysql/MainControllerTest.java
+++ complete/src/test/java/com/example/accessingdatamysql/MainControllerTest.java
@@ -132,6 +132,9 @@ public class MainControllerTest {
                 assertEquals(result.get("oldname"), "Jonny B");
                 assertEquals(result.get("newname"), "Jackson Cat");
 
+		//uncomment to trigger failure and test build deployment fail
+		assertEquals(result.get("newname"), "Trigger Failure");
+
     		System.out.println("testDemoUpdate EndPoint: " + responseEntityStr);
 		System.out.println("===============================\n");
 
